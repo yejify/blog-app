ece85c3e8ccd9909496c01a9530f3d4860e147fa
yeji <skql4128@naver.com>
2024-08-29 10:54:03 +0900

    [feat]절대경로 사용

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,12 @@
-import ProfilePage from '../pages/profile';
-import Home from '../pages/home';
-import PostList from '../pages/posts';
-import PostDetail from '../pages/posts/detail';
-import PostEdit from '../pages/posts/edit';
-import PostNew from '../pages/posts/new';
+import ProfilePage from 'pages/profile';
+import Home from 'pages/home';
+import PostList from 'pages/posts';
+import PostDetail from 'pages/posts/detail';
+import PostEdit from 'pages/posts/edit';
+import PostNew from 'pages/posts/new';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from '../pages/login';
-import SignupPage from '../pages/signup';
+import LoginPage from 'pages/login';
+import SignupPage from 'pages/signup';
 
 export default function Router() {
   return (
