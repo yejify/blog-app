270a4c5480d6e5c39277c66e5b36b954faf90150
yeji <skql4128@naver.com>
2024-09-10 16:48:33 +0900

    [feat] Firebase Auth setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import Router from './components/Router';
+import { app } from 'firebaseApp';
+import { getAuth } from 'firebase/auth';
 
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <>
-      <Router />
+      <Router isAuthenticated={isAuthenticated} />
     </>
   );
 }
