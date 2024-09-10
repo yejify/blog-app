242dfda72f79fcf0e32086dd5bbfef5f7eea7b11
yeji <skql4128@naver.com>
2024-09-10 16:47:24 +0900

    [feat] router 로그인 경로 설정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,26 +1,39 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
 import ProfilePage from 'pages/profile';
 import Home from 'pages/home';
 import PostList from 'pages/posts';
 import PostDetail from 'pages/posts/detail';
 import PostEdit from 'pages/posts/edit';
 import PostNew from 'pages/posts/new';
-import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from 'pages/login';
 import SignupPage from 'pages/signup';
 
-export default function Router() {
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<PostList />} />
-        <Route path='/posts/:id' element={<PostDetail />} />
-        <Route path='/posts/new' element={<PostNew />} />
-        <Route path='/posts/edit/:id' element={<PostEdit />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='*' element={<Navigate replace to='/' />} />
+        {isAuthenticated ? (
+          <>
+            <Route path='/' element={<Home />} />
+            <Route path='/posts' element={<PostList />} />
+            <Route path='/posts/:id' element={<PostDetail />} />
+            <Route path='/posts/new' element={<PostNew />} />
+            <Route path='/posts/edit/:id' element={<PostEdit />} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='*' element={<Navigate replace to='/' />} />
+          </>
+        ) : (
+          <>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='*' element={<LoginPage />} />
+          </>
+        )}
       </Routes>
     </>
   );
