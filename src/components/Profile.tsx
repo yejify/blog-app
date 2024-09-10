e9c125e64dfb484321d81e12333d22314723be44
yeji <skql4128@naver.com>
2024-09-10 22:03:33 +0900

    [feat]logout 구현

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,35 @@
+import { getAuth, signOut } from 'firebase/auth';
+import { app } from 'firebaseApp';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Profile() {
+  const auth = getAuth(app);
+
+  const onSignOut = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+      toast.success('로그아웃 되었습니다.');
+    } catch (error: any) {
+      toast.error(error?.code);
+    }
+  };
+
   return (
     <div className='profile__box'>
       <div className='flex__box-lg'>
         <div className='profile__image' />
         <div>
-          <div className='profile__email'>test@test.com</div>
-          <div className='profile__name'>jiji</div>
+          <div className='profile__email'>{auth?.currentUser?.email}</div>
+          <div className='profile__name'>
+            {auth?.currentUser?.displayName || '사용자'}
+          </div>
         </div>
       </div>
-      <Link to='/' className='profile__logout'>
+      <div role='presentation' className='profile__logout' onClick={onSignOut}>
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 }
