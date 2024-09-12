c84eca31f9ca0874e42f55120e804579a2bebce7
yeji <skql4128@naver.com>
2024-09-12 17:57:14 +0900

    [fix] button 수정

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -44,7 +44,13 @@ export default function PostDetail() {
             <div className='post__date'>{post?.createdAt}</div>
           </div>
           <div className='post__utils-box'>
-            <div className='post_delete'>삭제</div>
+            <div
+              className='post__delete'
+              role='presentation'
+              onClick={handleDelete}
+            >
+              삭제
+            </div>
             <div className='post__edit'>
               <Link to={`/posts/edit/${post?.id}`}>수정</Link>
             </div>
