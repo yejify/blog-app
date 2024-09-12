a969fb279be0b448bdfd4a650e0d52e946d63071
yeji <skql4128@naver.com>
2024-09-13 08:12:22 +0900

    [feat] dark mode theme 기능구현

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, createContext, useState } from 'react';
+
+const ThemeContext = createContext({
+  theme: 'light',
+  toggleMode: () => {},
+});
+
+interface ThemeProps {
+  children: ReactNode;
+}
+
+export const ThemeContextProvider = ({ children }: ThemeProps) => {
+  const [theme, setTheme] = useState('light');
+  window.localStorage.getItem('theme') || 'light';
+
+  const toggleMode = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+    window.localStorage.setItem('theme', theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
