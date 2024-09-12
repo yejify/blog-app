f934113877e248d7e92785c25ec193a654a3f48f
yeji <skql4128@naver.com>
2024-09-12 17:15:02 +0900

    [feat] post delete 기능 구현

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { PostProps } from './PostList';
-import { doc, getDoc } from 'firebase/firestore';
+import { deleteDoc, doc, getDoc } from 'firebase/firestore';
 import { db } from 'firebaseApp';
+import { toast } from 'react-toastify';
 
 export default function PostDetail() {
-  const [posts, setPosts] = useState<PostProps | null>(null);
+  const [post, setPost] = useState<PostProps | null>(null);
   const params = useParams();
+  const navigate = useNavigate();
 
   const getPosts = async (id: string) => {
     if (id) {
       const docRef = doc(db, 'posts', id);
       const docSnap = await getDoc(docRef);
 
-      setPosts({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  const handleDelete = async () => {
+    const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?');
+
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, 'posts', post.id));
+      toast.success('게시글을 삭제했습니다.');
+      navigate('/');
     }
   };
 
@@ -25,21 +37,19 @@ export default function PostDetail() {
     <>
       <div className='post__detail'>
         <div className='post__box'>
-          <div className='post__title'>{posts?.title}</div>
+          <div className='post__title'>{post?.title}</div>
           <div className='post__profile-box'>
             <div className='post__profile' />
-            <div className='post__author-name'>{posts?.email}</div>
-            <div className='post__date'>{posts?.createdAt}</div>
+            <div className='post__author-name'>{post?.email}</div>
+            <div className='post__date'>{post?.createdAt}</div>
           </div>
           <div className='post__utils-box'>
             <div className='post_delete'>삭제</div>
             <div className='post__edit'>
-              <Link to={`/posts/edit/${posts?.id}`}>수정</Link>
+              <Link to={`/posts/edit/${post?.id}`}>수정</Link>
             </div>
           </div>
-          <div className='post__text post__text--pre-wrap'>
-            {posts?.content}
-          </div>
+          <div className='post__text post__text--pre-wrap'>{post?.content}</div>
         </div>
       </div>
     </>
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,8 +1,9 @@
 import AuthContext from 'context/AuthContext';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import { db } from 'firebaseApp';
 import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 interface PostListProps {
   hasNavigation?: boolean;
@@ -23,16 +24,27 @@ export interface PostProps {
 
 export default function PostList({ hasNavigation = true }: PostListProps) {
   const [activeTab, setActiveTab] = useState<TabType>('all');
-  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [post, setPost] = useState<PostProps[]>([]);
   const { user } = useContext(AuthContext);
 
   const getPosts = async () => {
     const datas = await getDocs(collection(db, 'posts'));
+    setPost([]);
 
     datas?.forEach((doc) => {
       const dataObj = { ...doc.data(), id: doc.id };
-      setPosts((prev) => [...prev, dataObj as PostProps]);
+      setPost((prev) => [...prev, dataObj as PostProps]);
     });
+  };
+
+  const handleDelete = async (id: string) => {
+    const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?');
+    if (confirm && id) {
+      await deleteDoc(doc(db, 'posts', id));
+
+      toast.success('게시글을 삭제했습니다.');
+      getPosts();
+    }
   };
 
   useEffect(() => {
@@ -60,23 +72,29 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
         </div>
       )}
       <div className='post__list'>
-        {posts?.length > 0 ? (
-          posts.map((posts, index) => (
-            <div key={posts?.id} className='post__box'>
-              <Link to={`/posts/${posts?.id}`}>
+        {post?.length > 0 ? (
+          post.map((post, index) => (
+            <div key={post?.id} className='post__box'>
+              <Link to={`/posts/${post?.id}`}>
                 <div className='post__profile-box'>
                   <div className='post__profile' />
-                  <div className='post__author-name'>{posts?.email}</div>
-                  <div className='post__date'>{posts?.createdAt}</div>
+                  <div className='post__author-name'>{post?.email}</div>
+                  <div className='post__date'>{post?.createdAt}</div>
                 </div>
-                <div className='post__title'>{posts?.title}</div>
-                <div className='post__text'>{posts?.summary}</div>
+                <div className='post__title'>{post?.title}</div>
+                <div className='post__text'>{post?.summary}</div>
               </Link>
 
-              {posts?.email === user?.email && (
+              {post?.email === user?.email && (
                 <div className='post__utils-box'>
-                  <div className='post__delete'>삭제</div>
-                  <Link to={`/posts/edit/${posts?.id}`} className='post__edit'>
+                  <div
+                    className='post__delete'
+                    role='presentation'
+                    onClick={() => handleDelete(post.id as string)}
+                  >
+                    삭제
+                  </div>
+                  <Link to={`/posts/edit/${post?.id}`} className='post__edit'>
                     수정
                   </Link>
                 </div>
