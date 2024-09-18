cda5f3a12e1eda7adc2b995c5d433ddf7a9978d9
yeji <skql4128@naver.com>
2024-09-18 16:01:29 +0900

    [feat] 게시글 댓글 생성 기능 구현

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,9 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { PostProps } from './PostList';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import AuthContext from 'context/AuthContext';
+import { toast } from 'react-toastify';
 
 const COMMENTS = [
   {
@@ -21,8 +26,13 @@ const COMMENTS = [
   },
 ];
 
-export default function Comments() {
+interface CommentsProps {
+  post: PostProps;
+}
+
+export default function Comments({ post }: CommentsProps) {
   const [comment, setComment] = useState('');
+  const { user } = useContext(AuthContext);
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const {
@@ -34,9 +44,44 @@ export default function Comments() {
     }
   };
 
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if (post && post.id) {
+        const postRef = doc(db, 'posts', post.id);
+
+        if (user?.uid) {
+          const commentObj = {
+            content: comment,
+            uid: user.uid,
+            email: user.email,
+            createdAt: new Date()?.toLocaleDateString('ko', {
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+            }),
+          };
+          await updateDoc(postRef, {
+            comments: arrayUnion(commentObj),
+            updateDated: new Date()?.toLocaleDateString('ko', {
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+            }),
+          });
+        }
+      }
+      toast.success('댓글을 생성했습니다.');
+      setComment('');
+    } catch (e: any) {
+      toast.error(e?.code);
+    }
+  };
+
   return (
     <div className='comments'>
-      <form className='comments__form'>
+      <form className='comments__form' onSubmit={onSubmit}>
         <div className='form__block'>
           <label className='comment'>댓글 입력</label>
           <textarea
--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -72,7 +72,7 @@ export default function PostDetail() {
                 {post?.content}
               </div>
             </div>
-            <Comments />
+            <Comments post={post} />
           </>
         ) : (
           <Loader />
